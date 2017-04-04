a49598e7d6f2689affc8bb32f22f8004ddd3a5e7
candresantos <candre.santos@hotmail.com>
2017-04-04 11:55:22 +0000

    Update memoria.c

--- so_store/src/memoria.c
+++ so_store/src/memoria.c
@@ -23,6 +23,12 @@
 #include "ficheiro.h"
 #include "tempo.h"
 
+//==============================================
+// DECLARAR ACESSO A DADOS EXTERNOS
+//
+extern struct configuracao Config;
+//==============================================
+
 struct recibo_r BRecibo;  // buffer loja-cliente
 struct pedido_e BEncomenda; // buffer assistente-loja
 struct pedido_p BProduto; 	// buffer cliente-assistente
@@ -34,6 +40,10 @@ void * memoria_criar(char * name, int size) {
 	//==============================================
 	// FUNÇÃO GENÉRICA DE CRIAÇÃO DE MEMÓRIA PARTILHADA
 	//
+	// usar getuid() para gerar nome unico na forma:
+	// sprintf(name_uid,"/%s_%d", name, getuid())
+	// usar name_uid em shm_open
+	// usar tambem: ftruncate e mmap
 	return so_memoria_criar(name, size);
 	//==============================================
 }
@@ -42,6 +52,9 @@ void memoria_criar_stock() {
 	// CRIAR ZONA DE MEMÓRIA PARA O STOCK DE CADO PRODUTO
 	//
 	// utilizar a função genérica memoria_criar(char *,int)
+	// para criar ponteiro que se guarda em Config.stock
+	// que deve ter capacidade para um vetor unidimensional
+	// com a dimensao [PRODUTOS] para inteiro
 	so_memoria_criar_stock();
 	//==============================================
 }
@@ -50,6 +63,26 @@ void memoria_criar_buffers() {
 	// CRIAR ZONAS DE MEMÓRIA PARA OS BUFFERS: PEDIDOS DE PRODUTO, PEDIDOS DE ENCOMENDA e RECIBOS
 	//
 	// utilizar a função genérica memoria_criar(char *,int)
+	//
+	// para criar ponteiro que se guarda em BRecibo.ptr
+	// que deve ter capacidade para um vetor unidimensional
+	// de inteiros com a dimensao [BUFFER_RECIBO]
+	// para criar ponteiro que se guarda em BRecibo.buffer
+	// que deve ter capacidade para um vetor unidimensional
+	// com a dimensao [BUFFER_RECIBO] para struct produto
+	//
+	// para criar ponteiro que se guarda em BEncomenda.ptr
+	// que deve ter capacidade para um vetor unidimensional
+	// de inteiros com a dimensao [BUFFER_ENCOMENDA]
+	// para criar ponteiro que se guarda em BEncomenda.buffer
+	// que deve ter capacidade para um vetor unidimensional
+	// com a dimensao [BUFFER_ENCOMENDA] para struct produto
+	//
+	// para criar ponteiro que se guarda em BProduto.ptr
+	// que deve ter capacidade para struct ponteiros
+	// para criar ponteiro que se guarda em BProduto.buffer
+	// que deve ter capacidade para um vetor unidimensional
+	// com a dimensao [BUFFER_PEDIDO] para struct produto
 	so_memoria_criar_buffers();
 	//==============================================
 }
@@ -58,6 +91,9 @@ void memoria_criar_escalonador() {
 	// CRIAR ZONA DE MEMÓRIA PARA O MAPA DE ESCALONAMENTO
 	//
 	// utilizar a função genérica memoria_criar(char *,int)
+	// para criar ponteiro que se guarda em Escalonamento.ptr
+	// que deve ter capacidade para um vetor bidimensional
+	// com a dimensao [PRODUTOS,LOJAS] para inteiro
 	so_memoria_criar_escalonador();
 	//==============================================
 }
@@ -66,6 +102,10 @@ void memoria_terminar(char * name, void * ptr, int size) {
 	//==============================================
 	// FUNÇÃO GENÉRICA DE DESTRUIÇÃO DE MEMÓRIA PARTILHADA
 	//
+	// usar getuid() para gerar nome unico na forma:
+	// sprintf(name_uid,"/%s_%d", name, getuid())
+	// usar name_uid em shm_unlink
+	// usar tambem: munmap
 	so_memoria_terminar(name, ptr, size);
 	//==============================================
 }
@@ -83,7 +123,7 @@ void memoria_destruir() {
 }
 
 //******************************************
-// MEMORIA_PEDIDO_S_ESCREVE
+// MEMORIA_PEDIDO_P_ESCREVE
 //
 void memoria_pedido_p_escreve(int id, struct produto *pProduto) {
 	prodcons_pedido_p_produzir_inicio();
@@ -94,6 +134,11 @@ void memoria_pedido_p_escreve(int id, struct produto *pProduto) {
 	//==============================================
 	// ESCREVER PEDIDO DE PRODUTO NO BUFFER PEDIDO DE PRODUTOS
 	//
+	// copiar conteudo relevante da estrutura pProduto para
+	// a posicao BProduto.ptr->in do buffer BProduto
+	// conteudo: cliente, id, hora_pedido
+	// colocar disponivel = 1 nessa posicao do BProduto
+	// e atualizar BProduto.ptr->in
 	so_memoria_pedido_p_escreve(id, pProduto);
 	//==============================================
 
@@ -106,7 +151,7 @@ void memoria_pedido_p_escreve(int id, struct produto *pProduto) {
 	ficheiro_escrever_log_ficheiro(1, id);
 }
 //******************************************
-// MEMORIA_PEDIDO_S_LE
+// MEMORIA_PEDIDO_P_LE
 //
 int memoria_pedido_p_le(int id, struct produto *pProduto) {
 	// testar se existem clientes e se o SO_STORE esta aberto
@@ -118,6 +163,11 @@ int memoria_pedido_p_le(int id, struct produto *pProduto) {
 	//==============================================
 	// LER PEDIDO DE PRODUTO DO BUFFER PEDIDO DE PRODUTOS
 	//
+	// copiar conteudo relevante da posicao BProduto.ptr->out 
+	// do buffer BProduto para a estrutura pProduto
+	// conteudo: cliente, id, hora_pedido
+	// colocar disponivel = 0 nessa posicao do BProduto
+	// e atualizar BProduto.ptr->out
 	so_memoria_pedido_p_le(id, pProduto);
 	//==============================================
 
@@ -138,7 +188,7 @@ int memoria_pedido_p_le(int id, struct produto *pProduto) {
 }
 
 //******************************************
-// MEMORIA_PEDIDO_I_ESCREVE
+// MEMORIA_PEDIDO_E_ESCREVE
 //
 void memoria_pedido_e_escreve(int id, struct produto *pProduto) {
 	int pos, loja;
@@ -151,6 +201,11 @@ void memoria_pedido_e_escreve(int id, struct produto *pProduto) {
 	//==============================================
 	// ESCREVER PEDIDO NO BUFFER DE PEDIDOS DE ENCOMENDA
 	//
+	// procurar posicao vazia no buffer BEncomenda
+	// copiar conteudo relevante da estrutura pProduto para
+	// a posicao BEncomenda.ptr->in do buffer BEncomenda
+	// conteudo: cliente, id, disponivel, assistente, loja, hora_pedido
+	// colocar BEncomenda.ptr[n] = 1 na posicao respetiva
 	pos = so_memoria_pedido_e_escreve(id, pProduto, loja);
 	//==============================================
 
@@ -166,7 +221,7 @@ void memoria_pedido_e_escreve(int id, struct produto *pProduto) {
 	ficheiro_escrever_log_ficheiro(3, id);
 }
 //******************************************
-// MEMORIA_PEDIDO_LE
+// MEMORIA_PEDIDO_E_LE
 //
 int memoria_pedido_e_le(int id, struct produto *pProduto) {
 	// testar se existem pedidos e se o SO_Store esta aberto
@@ -178,6 +233,11 @@ int memoria_pedido_e_le(int id, struct produto *pProduto) {
 	//==============================================
 	// LER PEDIDO DO BUFFER DE PEDIDOS DE ENCOMENDA
 	//
+	// procurar pedido de encomenda para a loja no buffer BEncomenda
+	// copiar conteudo relevante da posicao encontrada
+	// no buffer BEncomenda para pProduto
+	// conteudo: cliente, id, disponivel, assistente, hora_pedido, hora_encomenda
+	// colocar BEncomenda.ptr[n] = 0 na posicao respetiva
 	so_memoria_pedido_e_le(id, pProduto);
 	//==============================================
 
@@ -190,7 +250,7 @@ int memoria_pedido_e_le(int id, struct produto *pProduto) {
 }
 
 //******************************************
-// MEMORIA_RECIBO_C_ESCREVE
+// MEMORIA_RECIBO_R_ESCREVE
 //
 void memoria_recibo_r_escreve(int id, struct produto *pProduto) {
 	int pos;
@@ -200,6 +260,11 @@ void memoria_recibo_r_escreve(int id, struct produto *pProduto) {
 	//==============================================
 	// ESCREVER RECIBO NO BUFFER DE RECIBOS
 	//
+	// procurar posicao vazia no buffer BRecibo
+	// copiar conteudo relevante da estrutura pProduto para
+	// a posicao BRecibo.ptr->in do buffer BRecibo
+	// conteudo: cliente, id, disponivel, assistente, loja, hora_pedido, hora_encomenda
+	// colocar BRecibo.ptr[n] = 1 na posicao respetiva
 	pos = so_memoria_recibo_r_escreve(id, pProduto);
 	//==============================================
 
@@ -215,11 +280,9 @@ void memoria_recibo_r_escreve(int id, struct produto *pProduto) {
 	ficheiro_escrever_log_ficheiro(5, id);
 }
 //******************************************
-// MEMORIA_RECIBO_C_LE
+// MEMORIA_RECIBO_R_LE
 //
 void memoria_recibo_r_le(int id, struct produto *pProduto) {
-	int n;
-
 	// aguardar recibo
 	controlo_cliente_aguarda_recibo(pProduto->cliente);
 
@@ -228,6 +291,11 @@ void memoria_recibo_r_le(int id, struct produto *pProduto) {
 	//==============================================
 	// LER RECIBO DO BUFFER DE RECIBOS
 	//
+	// procurar recibo para o cliente no buffer BRecibo
+	// copiar conteudo relevante da posicao encontrada
+	// no buffer BRecibo para pProduto
+	// conteudo: cliente, disponivel, assistente, loja, hora_pedido, hora_encomenda, hora_recibo
+	// colocar BRecibo.ptr[n] = 0 na posicao respetiva
 	so_memoria_recibo_r_le(id, pProduto);
 	//==============================================
 
@@ -248,10 +316,10 @@ void memoria_criar_indicadores() {
 	// para cada campo da estrutura indicadores
 	so_memoria_criar_indicadores();
 	// iniciar indicadores relevantes:
-	// - Ind.stock_inicial
-	// - Ind.clientes_atendidos_pelas_assistentes
-	// - Ind.clientes_atendidos_pelas_lojas
-	// - Ind.produtos_obtidos_pelos_clientes
+	// - Ind.stock_inicial (c/ Config.stock respetivo)
+	// - Ind.clientes_atendidos_pelas_assistentes (c/ 0)
+	// - Ind.clientes_atendidos_pelas_lojas (c/ 0)
+	// - Ind.produtos_obtidos_pelos_clientes (c/ 0)
 	so_memoria_iniciar_indicadores();
 	//==============================================
 }
